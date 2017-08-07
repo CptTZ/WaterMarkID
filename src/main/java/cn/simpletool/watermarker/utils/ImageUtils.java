ac9feaabe7abdaff74d3b41e99819420b34f3681
zhanjingbo <412008196@qq.com>
2017-08-07 22:21:38 +0800

    第一版修改，重写核心功能

--- src/main/java/cn/simpletool/watermarker/utils/ImageUtils.java
+++ src/main/java/cn/simpletool/watermarker/utils/ImageUtils.java
@@ -22,82 +22,60 @@ import java.io.*;
 public class ImageUtils {
 
     @Value("${imageUtil.fontName}")
-    private  String fontName;
+    private String fontName;
     @Value("${imageUtil.fontSize}")
-    private  int fontSize;
+    private int fontSize;
     @Value("${imageUtil.alpha}")
-    private  float alpha;
+    private float alpha;
     @Value("${imageUtil.carelessness}")
     private boolean carelessness;
     private static final String OUTPUT_FORMAT = "JPG";
 
-    public boolean waterMark(String markText,InputStream sourceImage,OutputStream targetImage){
-        return waterMark(markText,sourceImage,targetImage,fontName,fontSize,alpha,carelessness);
+    public boolean waterMark(String markText, InputStream sourceImage, OutputStream targetImage) {
+        return waterMark(markText, sourceImage, targetImage, fontName, fontSize, alpha, carelessness);
     }
-    public boolean waterMark(String markText, InputStream sourceImage, OutputStream targetImage, String fontName, int fontSize, float alpha, boolean carelessness){
-        BufferedImage bufferedImage = pressText(markText,sourceImage,fontName,fontSize,alpha,carelessness);
+
+    public boolean waterMark(String markText, InputStream sourceImage, OutputStream targetImage, String fontName, int fontSize, float alpha, boolean carelessness) {
+        BufferedImage bufferedImage = pressText(markText, sourceImage, fontName, fontSize, alpha, carelessness);
         try {
-            return ImageIO.write(bufferedImage,"JPG",targetImage);
+            return ImageIO.write(bufferedImage, "JPG", targetImage);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return false;
     }
 
-    private  BufferedImage pressText(String pressText, InputStream srcImg,String fontName,int fontSize,float alpha,boolean carelessness) {
+    private BufferedImage pressText(String pressText, InputStream src, String fontName, int fontSize, float alpha, boolean carelessness) {
         try {
-            Image src = ImageIO.read(srcImg);
-            int width = src.getWidth(null);
-            int height = src.getHeight(null);
+            Image srcImage = ImageIO.read(src);
+            BufferedImage targetImage = new BufferedImage(srcImage.getWidth(null), srcImage.getHeight(null), BufferedImage.TYPE_INT_RGB);
+            int height = srcImage.getHeight(null);
+            int width = srcImage.getWidth(null);
+            // 2、得到画笔对象
+            Graphics2D g = targetImage.createGraphics();
+            // 3、设置对线段的锯齿状边缘处理
+            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g.drawImage(srcImage.getScaledInstance(srcImage.getWidth(null), srcImage.getHeight(null), Image.SCALE_SMOOTH), 0, 0, null);
 
-            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-            Graphics2D g2d = image.createGraphics();
-            //绘原图
-            g2d.drawImage(src, 0, 0, width, height, null);
-            //比例
-            g2d.scale(1, 1);
-
-            g2d.addRenderingHints(new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON));
-            g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-
-            //颜色
-            Color color = Color.white;
-            //字体
-            Font font = new Font(fontName, Font.PLAIN, fontSize);
-
-            GlyphVector fontGV = font.createGlyphVector(g2d.getFontRenderContext(), pressText);
-            Rectangle size = fontGV.getPixelBounds(g2d.getFontRenderContext(), 0, 0);
-            Shape textShape = fontGV.getOutline();
-            double textWidth = size.getWidth();
-            double textHeight = size.getHeight();
-            AffineTransform rotate45 = AffineTransform.getRotateInstance(Math.PI / 4d);
-            Shape rotatedText = rotate45.createTransformedShape(textShape);
-            // use a gradient that repeats 4 times
-            g2d.setPaint(new GradientPaint(0, 0, color,
-                    image.getWidth() / 2, image.getHeight() / 2, color));
-
-            //透明度
-            g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP,
-                    alpha));
-
-            // step in y direction is calc'ed using pythagoras + 5 pixel padding
-            double yStep = Math.sqrt(textWidth * textWidth / 2) + 5;
-            // step over image rendering watermark text
-            for (double x = -textWidth * 3; x < image.getWidth(); x += (textHeight * 3)) {
-                double y = -yStep;
-                for (; y < image.getHeight(); y += yStep) {
-                    g2d.draw(rotatedText);
-                    if (carelessness)//字体实心
-                    {
-                        g2d.fill(rotatedText);
-                    }
-                    g2d.translate(0, yStep);
+            g.rotate(Math.toRadians(45), (double) targetImage.getWidth() / 2, (double) targetImage.getHeight() / 2);
+            // 5、设置水印文字颜色
+            //g.setColor(fontColor);
+            // 6、设置水印文字Font
+            g.setFont(new Font(fontName, Font.PLAIN, fontSize));
+            // 7、设置水印文字透明度
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, alpha));
+            // 8、第一参数->设置的内容，后面两个参数->文字在图片上的坐标位置(x,y)
+            int yStep = (int) (fontSize * 1.8);
+            int xStep = fontSize * pressText.length() + 10;
+            for (int x = -xStep; x < width * 2; x += xStep) {
+                for (int y = -height; y < height * 2; y += yStep) {
+                    g.drawString(pressText, x, y);
                 }
-                g2d.translate(textHeight * 3, -(y + yStep));
             }
-            g2d.dispose();
-            return image;
+
+            // 9、释放资源
+            g.dispose();
+            return targetImage;
         } catch (IIOException e) {
             e.printStackTrace();
         } catch (Exception e) {
