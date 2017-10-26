1190862461a42e4e6feddeb844e4849c2246e15b
ZhanJingbo <412008196@qq.com>
2017-10-26 21:39:52 +0800

    修改

--- src/main/resources/static/assets/js/app.js
+++ src/main/resources/static/assets/js/app.js
@@ -1,43 +1,47 @@
 $(function () {
-  $('#doc-form-file').on('change', function (e) {
-    var file = e.target.files[0];
-    var reader = new FileReader();
-    if (file && file.type.match('image.*')) {
-      reader.readAsDataURL(file);
-    } else {
-      return false;
+    $('#doc-form-file').on('change', function (e) {
+        var file = e.target.files[0];
+        var reader = new FileReader();
+        if (file && file.type.match('image.*')) {
+            reader.readAsDataURL(file);
+        } else {
+            return false;
+        }
+        reader.onload = function (e) {
+            var data = e.target.result,
+                image = new Image();
+
+            image.src = data;
+            image.onload = function () {
+                WaterMark.mark({
+                    text: $("#markText").val(),
+                    id: "myCanvas",
+                    color: '#fff',
+                    xStart: 0,
+                    yStart: -1000,
+                    xSpace: 20,
+                    ySpace: 30,
+                    size: 18,
+                    rotate: 45,
+                    opacity: 0.8,
+                    width: image.width,
+                    height: image.height,
+                    imgUrl: data
+                });
+            };
+            setTimeout(function () {
+                $("#result").attr("src", document.getElementById("myCanvas").toDataURL("image/png"));
+            }, 100);
+        }
+
+    });
+
+    $('#download').on('click', function () {
+        downloadCanvas(this, 'myCanvas', 'SimpleTool.png');
+    });
+
+    function downloadCanvas(link, canvasId, filename) {
+        link.href = document.getElementById(canvasId).toDataURL();
+        link.download = filename;
     }
-    reader.onload = function (e) {
-      var data = e.target.result,
-        image = new Image();
-
-      image.src = data;
-      image.onload = function () {
-        WaterMark.mark({
-          text: "此证件仅供办理xx业务使用，他用无效",
-          id: "myCanvas",
-          color: '#fff',
-          xStart: 0,
-          yStart: -1000,
-          xSpace: 20,
-          ySpace: 30,
-          size: 18,
-          rotate: 45,
-          opacity: 0.8,
-          width: image.width,
-          height: image.height,
-          imgUrl: data
-        });
-      };
-    }
-  });
-
-  $('#download').on('click', function () {
-    downloadCanvas(this, 'myCanvas', 'test.png');
-  });
-
-  function downloadCanvas(link, canvasId, filename) {
-    link.href = document.getElementById(canvasId).toDataURL();
-    link.download = filename;
-  }
 });
--- src/main/resources/templates/index_new.ftl
+++ src/main/resources/templates/index_new.ftl
@@ -13,31 +13,30 @@
                 <div class="am-form-group">
                     <label for="markText" class="am-u-lg-2 am-u-sm-3 am-form-label">水印文字:</label>
                     <div class="am-u-lg-10 am-u-sm-9">
-                        <input  name="markText" class="am-form-field am-radius" id="markText"
-                                placeholder="此证件仅供办理xx业务使用，他用无效"/>
+                        <input name="markText" class="am-form-field am-radius" id="markText"
+                               value="此证件仅供办理xx业务使用，他用无效"/>
                     </div>
                 </div>
                 <div class="am-form-group am-form-file am-u-sm-12">
                     <button type="button" class="am-btn am-btn-default am-btn-block am-round">
                         <i class="am-icon-cloud-upload"></i> 上传证件图片
                     </button>
-                    <input id="doc-form-file" type="file" name="srcImage" multiple="multiple" accept="image/png,image/jpg,image/jpeg,image/bmp"/>
+                    <input id="doc-form-file" type="file" name="srcImage" multiple="multiple"
+                           accept="image/png,image/jpg,image/jpeg,image/bmp"/>
                 </div>
                 <div class="am-form-group am-form-file am-u-sm-12">
-                    <button type="button" class="am-btn am-btn-primary am-round am-btn-block">生成水印 </button>
+                    <a type="button" id="download" class="am-btn am-btn-primary am-round am-btn-block"><i class="am-icon-save"></i>  保存图片</a>
                 </div>
             </fieldset>
         </form>
     </div>
     <div class="am-u-lg-12 am-u-md-8 am-u-sm-centered example">
-        <img src="/assets/image/example.jpg" />
-        <div class="text">水印效果示意图</div>
+        <img id="result" src="/assets/image/example.jpg"/>
+        <div class="text">水印效果实时预览</div>
     </div>
-    <canvas id="myCanvas"></canvas>
-    <a id="download">点击下载</a>
 </div>
 <div class="am-container">
-    <p class="footer">© 2017 Simple Tool. 联系我们:<a href="mailto:simpletool@126.com">simpletool@126.com</a></p>
+    <p class="footer">浙ICP备17043803号 © 2017 Simple Tool. 联系我们:<a href="mailto:simpletool@126.com">simpletool@126.com</a></p>
 </div>
 <!--[if lt IE 9]>
 <script src="http://libs.baidu.com/jquery/1.11.1/jquery.min.js"></script>
@@ -51,5 +50,6 @@
 <script src="assets/js/amazeui.min.js"></script>
 <script src="assets/js/waterMark.js"></script>
 <script src="assets/js/app.js"></script>
+<canvas id="myCanvas"></canvas>
 </body>
 <#include "common/footer.ftl"/>
