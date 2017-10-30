f033fe2686e6a63c06c1b56b496631a24a856586
ZhanJingbo <412008196@qq.com>
2017-10-30 14:52:06 +0800

    v0.3 计划实现前后端分离，先对前端资源进行抽取，逐步修改

--- src/main/resources/static/assets/js/amazeui.js
+++ src/main/resources/static/assets/js/amazeui.js
@@ -11303,7 +11303,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	  qq: {
 	    title: 'QQ 好友',
-	    url: 'http://connect.qq.com/widget/shareqq/index.ftl',
+	    url: 'http://connect.qq.com/widget/shareqq/index_old.html',
 	    icon: 'qq'
 	  },
 	  // url:,
