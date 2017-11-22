776682e4473ce31be5ff146ffd5e3c664657a8c5
ZhanJingbo <412008196@qq.com>
2017-11-22 20:36:24 +0800

    0.3 发布脚本

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 mvn clean package -Dmaven.test.skip=true
 cp ./target/app.jar ~/app.jar
 cd ~
-nohup java -jar -Dspring.profiles.active=prod app.jar
+sudo nohup java -jar -Dspring.profiles.active=prod app.jar
