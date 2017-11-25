9d6d8fea758d40d0beceeda7343ba0b7b610b3a6
ZhanJingbo <412008196@qq.com>
2017-11-25 23:50:56 +0800

    0.3 发布脚本

--- publish.sh
+++ publish.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 mvn clean package -Dmaven.test.skip=true
 cp ./target/app.jar ~/app.jar
-cp -r ./webapp/dist/* /webRoot/
 cd ~
 sudo nohup java -jar -Dspring.profiles.active=prod app.jar
