d1877eec2a4e3d7d75d0664147e114f61ab879e5
guxinyan <guxinyan@192.168.0.7>
2017-12-27 23:37:54 +0800

    小程序卡顿修复

--- WaterMarker-Wechat-Front/pages/waterMarker/index.js
+++ WaterMarker-Wechat-Front/pages/waterMarker/index.js
@@ -4,6 +4,19 @@ const app = getApp();
 const WaterMarker = require('../../utils/waterMark.js');
 
 const WM = new WaterMarker();
+var Config = {
+  colorMap: [
+    ['0', '#ffffff'],
+    ['1', '#979797'],
+    ['2', '#000000']
+  ],
+  waterMarkConfig: {
+    color: '#ffffff',
+    opacity: 0.5
+  },
+  text: '此证件仅供办理XX业务使用，他用无效',
+  debounceDelay: 200
+};
 
 Page({
   data: {
@@ -11,22 +24,22 @@ Page({
     canvasH: 240,
     inputFocus: false
   },
-  onReady: function (event){
+  onReady: function (event) {
     this.getDefaultConfig();
   },
-  touchImgstart: function(event){
+  touchImgstart: function (event) {
     this.setData({
       touch: event.changedTouches[0]
     });
   },
-  touchImgend: function(event){
+  touchImgend: function (event) {
     var start = this.data.touch,
       end = event.changedTouches[0];
-    if (Math.abs(start.clientX - end.clientX) < 10 && Math.abs(start.clientY - end.clientY) < 10){
+    if (Math.abs(start.clientX - end.clientX) < 10 && Math.abs(start.clientY - end.clientY) < 10) {
       this.chooseImg();
     }
   },
-  chooseImg: function(event){
+  chooseImg: function (event) {
     var that = this;
     wx.chooseImage({
       count: 1,
@@ -34,7 +47,8 @@ Page({
         wx.getImageInfo({
           src: res1.tempFilePaths[0],
           success: function (res) {
-            var imgW = res.width, imgH = res.height;
+            var imgW = res.width,
+              imgH = res.height;
             var windowWidth = wx.getSystemInfoSync().windowWidth;
 
             var scale = windowWidth / imgW;
@@ -62,9 +76,9 @@ Page({
               size: 30,
               imgUrl: res1.tempFilePaths[0]
             };
-            config = Object.assign(config, that.data.waterMarkConfig);
+            config = Object.assign(config, Config.waterMarkConfig);
 
-            WM.mark(config).then(function(){
+            WM.mark(config).then(function () {
               that.log(1);
               that.setData({
                 draw: true
@@ -75,8 +89,8 @@ Page({
       }
     });
   },
-  sameImg: function (event){
-    if (!this.data.draw) return false;    
+  sameImg: function (event) {
+    if (!this.data.draw) return false;
     wx.canvasToTempFilePath({
       canvasId: 'myCanvas',
       destWidth: this.data.imgW,
@@ -84,19 +98,19 @@ Page({
       success: function (res) {
         wx.saveImageToPhotosAlbum({
           filePath: res.tempFilePath,
-          success: function(){
+          success: function () {
             wx.showToast({
               title: '已保存到相册',
               icon: 'success',
               duration: 2000
-            }) 
+            })
           }
         });
       }
     });
     this.log(3);
   },
-  previewImg: function(){
+  previewImg: function () {
     if (!this.data.draw) return false;
     wx.canvasToTempFilePath({
       canvasId: 'myCanvas',
@@ -123,12 +137,12 @@ Page({
       that.log(1);
     });
   },
-  handleTextChange: function(event){
-    if (this.data.draw)WM.reRendering({
+  handleTextChange: debounce(function (event) {
+    if (this.data.draw) WM.reRendering({
       text: event.detail.value
     });
-  },
-  handleTextFocus: function(event){
+  }, Config.debounceDelay),
+  handleTextFocus: function (event) {
     this.setData({
       inputFocus: true
     });
@@ -139,11 +153,11 @@ Page({
     this.setData({
       inputFocus: false,
       text: event.detail.value
-    }, function(){
+    }, function () {
       if (that.data.draw && prevText !== that.data.text) that.log(1);
     })
   },
-  log: function(type){
+  log: function (type) {
     var that = this;
     wx.request({
       url: 'https://www.simpletool.cn/api/log',
@@ -157,48 +171,65 @@ Page({
       }
     });
   },
-  getDefaultConfig: function(){
-    var config = {
-      colorMap: [
-       ['0', '#ffffff'],
-       ['1', '#979797'],
-       ['2', '#000000']
-      ],
-      waterMarkConfig: {
-        color: '#ffffff',
-        opacity: 0.5
-      },
-      text: '此证件仅供办理XX业务使用，他用无效'
-    };
-
+  getDefaultConfig: function () {
     var that = this;
     wx.request({
       url: 'https://www.simpletool.cn/api/waterConfig/2',
       dataType: 'json',
-      success: function(res){
-        if(res.statusCode == 200){
-          config = res.data;
+      success: function (res) {
+        if (res.statusCode == 200) {
+          Config = Object.assign(Config, res.data);
         }
       },
-      complete: function(){
-        var colorMap = config.colorMap,
-          waterMarkConfig = config.waterMarkConfig,
+      complete: function () {
+        var colorMap = Config.colorMap,
+          waterMarkConfig = Config.waterMarkConfig,
           defaultColor = colorMap.filter(function (color) {
             return color[1] == waterMarkConfig.color;
           });
         that.setData({
-          ColorMap: config.colorMap,
+          ColorMap: colorMap,
           currentColorIndex: defaultColor.length ? defaultColor[0][0] : -1,
-          defaultText: config.text,
-          text: config.text,
-          waterMarkConfig: config.waterMarkConfig
+          defaultText: Config.text,
+          text: Config.text
         });
       }
     });
   },
-  reward: function(){
+  reward: function () {
     wx.previewImage({
       urls: ['https://www.simpletool.cn/static/money.jpg']
     })
   }
 })
+
+function debounce(func, wait) {
+  var timeout, args, context, timestamp, result;
+
+  var later = function () {
+    var last = new Date().getTime() - timestamp;
+
+    if (last < wait && last >= 0) {
+      timeout = setTimeout(later, wait - last);
+    } else {
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    }
+  };
+
+  return function () {
+    context = this;
+    args = arguments;
+    timestamp = new Date().getTime();
+    if (!timeout) timeout = setTimeout(later, wait);
+    return result;
+  };
+};
+
+function extend(origin, target) {
+  for (var key in target) {
+    target.hasOwnProperty(key) && (origin[key] = target[key]);
+  }
+  return origin;
+}
